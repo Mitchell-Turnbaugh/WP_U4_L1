bc9f997a6a0c46f804e79e388a37e033eaccb638
Mitchell Turnbaugh <MitchellTurnbaughCProg@gmail.com>
2026-02-18 08:37:39 -0500

    Linked the SDP

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function encrypt(){
-    const input = document.getElementsByTagName("input")[0];
-    
+    const input = document.getElementsByTagName("input")[0].value;
+    const shift = document.getElementsByTagName("input")[1].value;
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    let newString = ""
+    for(i of input){
+        newString += alphabet
+    }
 }
