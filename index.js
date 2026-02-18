76bc5a59ba91718e8826dee9efa36dfa0b3a9468
Mitchell Turnbaugh <MitchellTurnbaughCProg@gmail.com>
2026-02-18 10:41:21 -0500

    Added decrypt but it does not fully work

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 function encrypt(){
     const input = document.getElementsByTagName("input")[0].value;
-    const shift = document.getElementsByTagName("input")[1].value;
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    let newString = ""
-    for(i of input){
-        newString += alphabet
+    const shift = Number(document.getElementsByTagName("input")[1].value);
+    const result = document.getElementById("result_text");
+    if(!Number.isInteger(shift)){
+        result.textContent = "Shift number must be an integer";
+        return;
     }
+    alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let newString = "";
+    for(i of input){
+        if(alphabet.includes(i)){
+            newString += alphabet[(alphabet.indexOf(i) + shift) % 26];
+        }else{
+            newString += i;
+        }
+    }
+    result.textContent = newString;
+}
+function decrypt(){
+    const input = document.getElementsByTagName("input")[0].value;
+    const shift = Number(document.getElementsByTagName("input")[1].value);
+    const result = document.getElementById("result_text");
+    if(!Number.isInteger(shift)){
+        result.textContent = "Shift number must be an integer";
+        return;
+    }
+    alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let newString = "";
+    for(i of input){
+        if(alphabet.includes(i)){
+            console.log((alphabet.indexOf(i) - shift) % 26)
+            newString += alphabet[(alphabet.indexOf(i) - shift) % 26];
+        }else{
+            newString += i;
+        }
+    }
+    result.textContent = newString;
 }
